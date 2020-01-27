aa291c1f4cb9130881165876b119baa5d1314b51
T. Jastrzebski <tdjastrzebski@wp.pl>
2020-01-27 22:01:54 +0100

    1.3.0

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # What is it?
 **Build++** is **VS Code** multi-step incremental build tool extension based on JSON, string templates and [glob syntax](https://en.wikipedia.org/wiki/Glob_(programming)).  
 **Build++** can build C/C++ projects using [vscode-cpptools](https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools) `includePath`, `defines` and `forcedInclude` variables combined with its own build steps but it is not limited to C/C++ builds.  
-**Build++** runs [CppBuild](https://github.com/tdjastrzebski/cppbuild/) command line utility using tasks it creates 'on the fly'.
+**Build++** runs [cppbuild](https://github.com/tdjastrzebski/cppbuild/) command line utility using tasks it creates 'on the fly'.
 
 # How to use it?
-1. Install command line [CppBuild](https://github.com/tdjastrzebski/cppbuild/) tool: `npm install cppbuild -g`
+1. Install command line [cppbuild](https://github.com/tdjastrzebski/cppbuild/) tool: `npm install cppbuild -g`
 1. Modify created `c_cpp_build.json` file according to your needs.  
 1. Go to menu **Terminal** and choose **Run Task...** option.  
 You should see additional build tasks available.  
 1. Build can be run from terminal window as well: `cppbuild <config name> [build type] -w`  
 `-w` switch tells **cppbuild** to use current VS Code workspace.
 
-Detailed `c_cpp_build.json` file description is available at [CppBuild](https://github.com/tdjastrzebski/cppbuild/) GitHub site.
+Detailed `c_cpp_build.json` file syntax is available at [cppbuild](https://github.com/tdjastrzebski/cppbuild/) GitHub site.
 
 # Notes
 1. Executing tasks **VS Code** uses current or default terminal. Therefore, using shell-specific commands in build steps make sure correct terminal shell is active or selected as default.
 1. Build can be run from command line as well. Example: `cppbuild GCC debug`
 1. If used with [vscode-cpptools](https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools) `c_cpp_build.json`, file must contain configurations named exactly as those defined in `c_cpp_properties.json` file.
-1. [CppBuild](https://github.com/tdjastrzebski/cppbuild/) is not limited to C/C++ builds and can be run without `c_cpp_properties.json` file. Use `-p` option with no file name, which requires [CppBuild](https://github.com/tdjastrzebski/cppbuild/) version 1.1.0 or later.
+1. [cppbuild](https://github.com/tdjastrzebski/cppbuild/) is not limited to C/C++ builds and can be run without `c_cpp_properties.json` file. Use `-p` option with no file name, which requires [cppbuild](https://github.com/tdjastrzebski/cppbuild/) version 1.1.0 or later.
 1. For more options use `cppbuild --help`.
 
 # Build file syntax
@@ -56,10 +56,11 @@ https://github.com/tdjastrzebski/cppbuild/
 https://github.com/tdjastrzebski/vscode-cppbuild
 
 # Known issues
-1. Although created tasks are marked as Build tasks, they do not appear in **Build Tasks** group. I suspect VS Code bug.
-1. Occasionally output parts are not visible in Terminal window. The problem does not occur if **cppbuild** is run from the command prompt. The problem may be related to multi threading.
+1. Although created tasks are marked as Build tasks, they do not appear in **Build Tasks** group. It is not clear whether this is a bug or not. For details refer to [VS Code issue #83378](https://github.com/microsoft/vscode/issues/83378#issuecomment-548838702).
+1. Occasionally, output parts are not visible in Terminal window. The problem does not occur if **cppbuild** is run from the command prompt. The problem may be related to multi threading.
 
 # Release notes
 * 1.0 Initial release
 * 1.1 `params` can be added on all levels, tool can work without C/C++ extension and `c_cpp_properties.json` file.
 * 1.2 Added support for incremental builds and `outputFile` build step property.
+* 1.3 [cppbuild](https://github.com/tdjastrzebski/cppbuild/) updated to significantly improved version 1.3.0.
--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,15 @@
 'use strict';
 
 import * as path from 'path';
-import * as fs from 'fs';
 import * as vscode from 'vscode';
-import * as cppt from 'cppbuild';
+import * as cppb from 'cppbuild';
 import * as semver from 'semver';
 import { TaskDefinition } from 'vscode';
 import { TaskDetector } from './TaskDetector';
 import { findToolCommand } from './vscode';
 
 const ExtensionName: string = "Build++";
-const MinToolVersion = '1.2.8';
+const MinToolVersion = '1.3.0';
 const MinNodeVersion = '10.0.0';
 let _taskDetector: TaskDetector;
 let _channel: vscode.OutputChannel;
@@ -40,19 +39,19 @@ export function activate(_context: vscode.ExtensionContext): void {
 		} else if (semver.gt(MinNodeVersion, nodeVersion)) {
 			logError(`Update node.js to version ${MinNodeVersion} or newer.`);
 		} else {
-			const cppbuildVersion = await getToolVersion(cppt.ToolName);
+			const cppbuildVersion = await getToolVersion(cppb.ToolName);
 
 			if (!cppbuildVersion) {
-				logError(`Install CppBuild: npm install ${cppt.ToolName} -g`);
+				logError(`Install CppBuild: npm install ${cppb.ToolName} -g`);
 			} else if (semver.gt(MinToolVersion, cppbuildVersion)) {
-				logError(`The minimum version of ${cppt.ToolName} is ${MinToolVersion} and you have ${cppbuildVersion}.\nUpdate it now: npm install ${cppt.ToolName} -g`);
+				logError(`The minimum version of ${cppb.ToolName} is ${MinToolVersion} and you have ${cppbuildVersion}.\nUpdate it now: npm install ${cppb.ToolName} -g`);
 			}
 		}
 
 		const buildStepsPath = getBuildStepsPath(rootFolder);
 		if (!buildStepsPath) return;
 
-		if (!await cppt.checkFileExists(buildStepsPath)) {
+		if (!await cppb.checkFileExists(buildStepsPath)) {
 			// build file does not exits yet
 			let value = await showDialog(rootFolder);
 
@@ -105,14 +104,14 @@ function getVscodeFolderPath(rootFolder: vscode.WorkspaceFolder): string | undef
 	if (!rootFolder) return undefined;
 	const rootPath = rootFolder.uri.scheme === 'file' ? rootFolder.uri.fsPath : undefined;
 	if (!rootPath) return undefined;
-	const vscodeFolderPath = path.join(rootPath, cppt.VscodeFolder);
+	const vscodeFolderPath = path.join(rootPath, cppb.VscodeFolder);
 	return vscodeFolderPath;
 }
 
 function getBuildStepsPath(rootFolder: vscode.WorkspaceFolder): string | undefined {
 	const vscodeFolderPath = getVscodeFolderPath(rootFolder);
 	if (!vscodeFolderPath) return undefined;
-	const buildStepsPath: string = path.join(vscodeFolderPath, cppt.BuildStepsFile);
+	const buildStepsPath: string = path.join(vscodeFolderPath, cppb.BuildStepsFile);
 	return buildStepsPath;
 }
 
@@ -139,99 +138,43 @@ async function createInitialBuildFile(rootFolder: vscode.WorkspaceFolder): Promi
 	if (!vscodeFolderPath) return false;
 	const buildStepsPath = getBuildStepsPath(rootFolder);
 	if (!buildStepsPath) return false;
-	if (await cppt.checkFileExists(buildStepsPath)) return false;
+	if (await cppb.checkFileExists(buildStepsPath)) return false;
 
-	const propertiesPath: string = path.join(vscodeFolderPath, cppt.PropertiesFile);
-	let cppConfig: cppt.ConfigurationJson | undefined;
+	const propertiesPath: string = path.join(vscodeFolderPath, cppb.PropertiesFile);
+	let cppConfig: cppb.ConfigurationJson | undefined;
 
-	if (await cppt.checkFileExists(propertiesPath)) {
-		cppConfig = cppt.getJsonObject(propertiesPath);
+	if (await cppb.checkFileExists(propertiesPath)) {
+		cppConfig = cppb.getJsonObject(propertiesPath);
 	}
 
-	const bConfigs: cppt.BuildConfiguration[] = [];
+	const stringToEnumValue = <ET, T>(enumObj: ET, str: string): T => (enumObj as any)[Object.keys(enumObj).filter(k => (enumObj as any)[k] === str)[0]];
 
 	if (cppConfig) {
-		cppConfig.configurations.forEach(c => {
-			let command: string;
-			let cmd: string | undefined;
-			const bSteps: cppt.BuildStep[] = [];
-			const bTypes: cppt.BuildType[] = [];
-			const problemMatchers: string[] = [];
-			let cParams: { [key: string]: string | string[] } | undefined;
-
-			switch (c.intelliSenseMode) {
-				case 'gcc-x64':
-					cmd = 'g++';
-				case 'clang-x64':
-					cmd = cmd || 'clang++';
-					bTypes.push({ name: 'debug', params: { buildTypeParams: '-O0 -g' } });
-					bTypes.push({ name: 'release', params: { buildTypeParams: '-O2 -g0' } });
-					command = cmd + ' -c -std=c++17 ${buildTypeParams} (-I[$${includePath}]) (-D$${defines}) (-include [$${forcedInclude}]) [${filePath}] -o [${outputFile}]';
-					bSteps.push({ name: 'C++ Compile Sample Step', filePattern: '**/*.cpp', outputFile: "${buildDir}/${buildTypeName}/${fileDirectory}/${fileName}.o", command: command });
-					command = cmd + ' [$${filePath}] -o [${buildDir}/${buildTypeName}/main.exe]';
-					bSteps.push({ name: 'C++ Link Sample Step', fileList: '${buildDir}/${buildTypeName}/**/*.o', command: command });
-					problemMatchers.push('$gcc');
-					break;
-				case 'msvc-x64':
-					bTypes.push({ name: 'debug', params: { buildTypeParams: '/MDd /Od /RTCsu /Zi /Fd[${buildDir}/${buildTypeName}/main.pdb]', linkTypeParams: '/DEBUG' } });
-					bTypes.push({ name: 'release', params: { buildTypeParams: '/MD /Ox', linkTypeParams: '' } });
-					command = 'cl.exe ${buildTypeParams} /nologo /EHs /GR /GF /W3 /EHsc /FS /c (/I[$${includePath}]) (/D\"$${defines}\") (/FI[$${forcedInclude}]) [${filePath}] /Fo[${outputFile}]';
-					bSteps.push({ name: 'C++ Compile Sample Step', filePattern: '**/*.cpp', outputFile: "${buildDir}/${buildTypeName}/${fileDirectory}/${fileName}.o", command: command });
-					command = 'link.exe /NOLOGO ${linkTypeParams} [$${filePath}] /OUT:[${buildDir}/${buildTypeName}/main.exe] /LIBPATH:[${ScopeCppSDK}/VC/lib] /LIBPATH:[${ScopeCppSDK}/SDK/lib]';
-					bSteps.push({ name: 'C++ Link Sample Step', fileList: '${buildDir}/${buildTypeName}/**/*.o', command: command });
-					problemMatchers.push('$msCompile');
-					cParams = { ScopeCppSDK: "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/SDK/ScopeCppSDK" };
+		for (const config of cppConfig.configurations) {
+			if (config.intelliSenseMode === undefined) continue; // skip this config
+			const compilerType = stringToEnumValue<typeof cppb.CompilerType, cppb.CompilerType>(cppb.CompilerType, config.intelliSenseMode); // convert string enum name to enum value
+			switch (compilerType) {
+				case cppb.CompilerType.gcc:
+				case cppb.CompilerType.clang:
+				case cppb.CompilerType.msvc:
+					await cppb.setSampleBuildConfig(buildStepsPath, config.name, compilerType);
 					break;
 				default:
-					return;
+					continue; // unsupported compiler type - skip this config
 			}
-			const bConfig: cppt.BuildConfiguration = { name: c.name, params: cParams, buildTypes: bTypes, buildSteps: bSteps, problemMatchers: problemMatchers };
-			bConfigs.push(bConfig);
-		});
-	} else {
-		let command: string;
-		const bSteps: cppt.BuildStep[] = [];
-		const bTypes: cppt.BuildType[] = [];
-		const problemMatchers: string[] = [];
-		let cParams: { [key: string]: string | string[] } | undefined;
 
-		bTypes.push({ name: 'debug', params: { buildTypeParams: '-O0 -g', defines: ["_DEBUG"] } });
-		bTypes.push({ name: 'release', params: { buildTypeParams: '-O2 -g0', defines: [] } });
-		command = 'g++ -c -std=c++17 ${buildTypeParams} (-D$${defines}) [${filePath}] -o [${outputFile}]';
-		bSteps.push({ name: 'C++ Compile Sample Step', filePattern: '**/*.cpp', outputFile: "${buildDir}/${buildTypeName}/${fileDirectory}/${fileName}.o", command: command });
-		command = 'g++ [$${filePath}] -o [${buildDir}/${buildTypeName}/main.exe]';
-		bSteps.push({ name: 'C++ Link Sample Step', fileList: '${buildDir}/${buildTypeName}/**/*.o', command: command });
-
-		const bConfig: cppt.BuildConfiguration = { name: "gcc", params: cParams, buildTypes: bTypes, buildSteps: bSteps, problemMatchers: problemMatchers };
-		bConfigs.push(bConfig);
-		problemMatchers.push('$gcc');
-	}
-
-	const bc: cppt.BuildConfigurations = { version: 1, params: { buildDir: "build/${configName}" }, configurations: bConfigs };
-	const text = JSON.stringify(bc, null, '\t');
-
-	if (!await cppt.checkDirectoryExists(vscodeFolderPath)) {
-		try {
-			await cppt.makeDirectory(vscodeFolderPath, { recursive: true });
-		} catch (e) {
-			logError(`Error creating ${vscodeFolderPath} folder.\n${e.message}`);
-			return false;
 		}
+	} else {
+		await cppb.setSampleBuildConfig(buildStepsPath, 'gcc', cppb.CompilerType.gcc);
+		await cppb.setSampleBuildConfig(buildStepsPath, 'clang', cppb.CompilerType.clang);
+		await cppb.setSampleBuildConfig(buildStepsPath, 'msvc', cppb.CompilerType.msvc);
 	}
-
-	try {
-		fs.writeFileSync(buildStepsPath, text);
-	} catch (e) {
-		logError(`Error writing ${buildStepsPath} file.\n${e.message}`);
-		return false;
-	}
-
 	return true;
 }
 
 async function getToolVersion(toolName: string): Promise<string | undefined> {
 	try {
-		const result = await cppt.execCmd(`${toolName} --version`, {});
+		const result = await cppb.execCmd(`${toolName} --version`, {});
 		if (result.error) return undefined;
 		let version = result.stdout.split(/[\r\n]/).filter(line => !!line)[0];
 		if (version.substr(0, 1) == 'v') version = version.substr(1);
@@ -243,13 +186,13 @@ async function getToolVersion(toolName: string): Promise<string | undefined> {
 
 function isEnabled(rootFolder: vscode.WorkspaceFolder): boolean {
 	// https://code.visualstudio.com/api/references/vscode-api
-	let result = vscode.workspace.getConfiguration(cppt.ToolName, rootFolder.uri).get<boolean>('isEnabled', true) === true;
+	let result = vscode.workspace.getConfiguration(cppb.ToolName, rootFolder.uri).get<boolean>('isEnabled', true) === true;
 	return result;
 }
 
 async function setEnabled(rootFolder: vscode.WorkspaceFolder, value: boolean) {
 	// https://code.visualstudio.com/api/references/vscode-api
-	await vscode.workspace.getConfiguration(cppt.ToolName, rootFolder.uri).update('isEnabled', value, false);
+	await vscode.workspace.getConfiguration(cppb.ToolName, rootFolder.uri).update('isEnabled', value, false);
 }
 
 async function computeTasks(workspaceFolder: vscode.WorkspaceFolder): Promise<vscode.Task[]> {
@@ -260,13 +203,13 @@ async function computeTasks(workspaceFolder: vscode.WorkspaceFolder): Promise<vs
 	if (!buildStepsPath) return tasks;
 	const vscodeFolderPath = getVscodeFolderPath(workspaceFolder);
 	if (!vscodeFolderPath) return tasks;
-	let propertiesPath: string | undefined = path.join(vscodeFolderPath, cppt.PropertiesFile);
-	let infos: cppt.BuildInfo[];
+	let propertiesPath: string | undefined = path.join(vscodeFolderPath, cppb.PropertiesFile);
+	let infos: cppb.BuildInfo[];
 
-	if (! await cppt.checkFileExists(propertiesPath)) propertiesPath = undefined;
+	if (! await cppb.checkFileExists(propertiesPath)) propertiesPath = undefined;
 
 	try {
-		infos = await cppt.getBuildInfos(buildStepsPath, propertiesPath);
+		infos = await cppb.getBuildInfos(buildStepsPath, propertiesPath);
 	} catch (e) {
 		const error = e as Error;
 		if (error) logError(error.message);
@@ -302,11 +245,11 @@ function buildTask(rootFolder: vscode.WorkspaceFolder, cmd: string, configName: 
 	const execution = new vscode.ShellExecution(cmd, args, options);
 	const kind: TaskDefinition = { type: 'shell' };
 	const name = `${configName}${buildType ? ' - ' + buildType : ''}`;
-	const task = new vscode.Task(kind, rootFolder, name, cppt.ToolName, execution);
+	const task = new vscode.Task(kind, rootFolder, name, cppb.ToolName, execution);
 	task.group = vscode.TaskGroup.Build; // this does not seem to work
-	task.source = cppt.ToolName;
+	task.source = cppb.ToolName;
 	// note: matcher needs to be specified, otherwise user has to select it from the list, task entry is created
 	// and extension stops working in debug - tasks list appears empty, probably it is VS Code bug
-	task.problemMatchers = problemMatchers || []; // FIXME: this does not seem to work
+	task.problemMatchers = problemMatchers || [];
 	return task;
 }
